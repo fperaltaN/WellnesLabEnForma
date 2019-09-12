9daa6db9422d4203c9580c858c5127f97d514d7e
fperaltaN <iscfperalta@gmail.com>
2019-09-11 19:52:18 -0500

    cumpleaños

---
+++ GymWebDeploy/GymWebDeploy/Controllers/CumpleanosController.cs
@@ -0,0 +1,71 @@
+﻿using GymWebDeploy.Models.Dao;
+using GymWebDeploy.Models.Domain;
+using GymWebDeploy.Models.Domain.Utils;
+using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace GymWebDeploy.Controllers
+{
+    public class CumpleanosController : Controller, IGenericController<Empleado>
+    {
+        LoginController status = new LoginController();
+        public JsonResult Get()
+        {
+            return Json(new GenericBaseDao().Get<Socio>(ConfigurationManager.AppSettings["QueryGETCumple"]), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetCumple()
+        {
+            return Json(new GenericBaseDao().Get<Socio>(ConfigurationManager.AppSettings["QueryGETCumple"]), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+
+            if (!status.checkSession())
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            LoginController logout = new LoginController();
+            logout.sessionAbandon();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public JsonResult Save(Empleado data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public JsonResult Update(Empleado data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public JsonResult sendBirthDayMail(String data)
+        {
+            try
+            {
+                Mail mail = new Mail();
+                mail.mail = data;
+                mail.send(true);
+                return Json(true);
+            }
+            catch (Exception ex)
+            {
+                return Json(false);
+            }
+        }
+    }
+}
---
+++ GymWebDeploy/GymWebDeploy/Scripts/Application/Cumpleanos.js
@@ -0,0 +1,56 @@
+﻿//Titulo para la exportación de archivos
+var FileTitle = 'Cumpleaños';
+// si crud true tiene grid si no es solo captura
+CRUD = true;
+note = false;
+//edit
+var nameEntity = 'Cumpleanos';
+///variable para el nombre del proyecto publicado puesto que cambian las rutas
+//dejar en blanco si se trabaja de manera local
+var publish = '';//'webdeploy/';
+//form //edit
+//edit
+var colModel = [
+    { label: 'id_socio', name: 'id_socio', width: 30, hidden: true },
+    { label: 'num_socio', name: 'num_socio', width: 40 },
+    { label: 'nombre', name: 'nombre', width: 50 },
+    { label: 'direccion', name: 'direccion', width: 30, hidden: true },
+    { label: 'mail', name: 'mail', width: 50 },
+    { label: 'Enviar', name: 'Enviar', width: 30, formatter: CustomCellOptionsCumple }
+];
+
+function CreateObject() {
+    data = {
+        id_socio: ''
+        , num_socio: ''
+        , nombre: ''
+        , direccion: ''
+        , mail: ''
+        , Enviar: 1
+    };
+}
+
+function CustomCellOptionsCumple(cellValue, options, rowdata, action) {
+    return '<button type="button" class="btn btn-sm btn-success" onclick="SendMail(&#39;' + rowdata.mail + '&#39;)">' +
+        '<img src="../' + publish + 'Content/icons/baseline-assignment_turned_in-white-18/1x/baseline_assignment_turned_in_white_18dp.png" />' +
+        '</button>';
+}
+
+function SendMail(socioMail) {
+    var path = '../' + publish + 'Cumpleanos/sendBirthDayMail/';
+    ajaxPostCall(path, ReturnJson(socioMail)).done(function (response) {
+        MsgSuccess('info', "El correo de felicitación ha sido enviado.");
+    });
+}
+
+function UpdateElementCumple(response) {
+    //updateTable(DataTableCumple, response);
+}
+
+$(document).ready(function () {
+    var path1 = '../' + publish + 'Cumpleanos/GetCumple/';
+    ajaxPostCall(path1, '').done(function (response) {
+        UpdateElementCumple(response);
+    });
+    $('#BtnAdd').toggle();
+});
